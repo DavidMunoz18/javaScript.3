01a3fb30417714306c1ede774cfa79ea163d838a
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2024-02-29 10:50:20 +0100

    Add files via upload

---
+++ javaScript.3/src/main/webapp/java.js
@@ -0,0 +1,15 @@
+let array1 = ["Hola", "Pepe", "Antonio"];
+
+document.write(array1);
+
+let array2 = prompt("Escribe: ")
+
+let a = [array2];
+
+let array3 = prompt("Escribe: ")
+
+let b = [array3];
+
+document.write(typeof(array2));
+
+document.write(typeof(array3));
